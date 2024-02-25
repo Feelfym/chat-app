7a672ecd82559f9f5c7e0f8a9b7978eb802e0b25
Feelfym <tomoya.noguchi@gmail.com>
2024-02-25 18:10:49 +0900

    ユーザー認証機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema[7.0].define(version: 2024_02_25_082349) do
   create_table "users", charset: "utf8", force: :cascade do |t|
-    t.string "user", default: "", null: false
+    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
